ff362a8a692bc36b99dfa40129b6d19b12375781
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-08-16 21:05:11 +0530

    Adding die rolling feature

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,7 @@
 public class SnakeLadder {
 	public static void main(String[] args) {
 		int position=0;
+		int die=(int)(Math.floor(Math.random()*10%6+1));
+		System.out.println("die="+die);
 	}
 }
