aa3b248184f7f7e2c7c8edc0eded4a4f85244d43
shubham01kumar1 <shubham01kumar1@gmail.com>
2021-08-16 23:02:32 +0530

    Adding winning position feature

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,19 +1,25 @@
 public class SnakeLadder {
+	
 	public static void main(String[] args) {
 		int position=0;
-		int die=(int)(Math.floor(Math.random()*10)%6)+1;
-		System.out.println("die="+die);
-		int option=(int)(Math.floor(Math.random()*10)%3)+1;
-		if (option==1) {
-			System.out.println("NO play,stays at same position");
+		while(position!=100) {
+			int die=(int)(Math.floor(Math.random()*10)%6)+1;
+			System.out.println("die="+die);
+			int option=(int)(Math.floor(Math.random()*10)%3)+1;		
+			if (option==1) {
+				System.out.println("NO play,stays at same position");
+			}
+			else if (option==2) {
+				System.out.println("Ladder, moving ahead by the position received on die");
+				position+=die;
+			}
+			else {
+				System.out.println("Snake, moving behind by the position received on die");
+				position-=die;
+				if (position<0)
+					position=0;
+			}
 		}
-		else if (option==2) {
-			System.out.println("Ladder, moving ahead by the position received on die");
-			position+=die;
-		}
-		else {
-			System.out.println("Snake, moving behind by the position received on die");
-			position-=die;
-		}
-	}
+		System.out.println("winning position is reached"+ position);
+	}	
 }
